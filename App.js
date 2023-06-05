17588351f99705ed4632ffe9c6b959a392c9e565
princid <princid85@gmail.com>
2023-06-05 18:16:45 +0530

    Importing Stack Navigator inside our main app

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import HomeScreen from "./screens/HomeScreen";
+import { StyleSheet } from "react-native";
+import { Provider } from "react-redux";
+import store from "./store";
+import StackNavigator from "./StackNavigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
+    <Provider store={store}>
+      <StackNavigator/>
+      <StatusBar style="auto" />
+    </Provider>
   );
 }
 
@@ -14,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
 });
