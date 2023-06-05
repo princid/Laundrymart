020ef41a9ba9f3e5919ea5b6a9dc125ca6a7ec15
princid <princid85@gmail.com>
2023-06-05 18:09:57 +0530

    Added carousel images

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import { SliderBox } from "react-native-image-slider-box";
 
