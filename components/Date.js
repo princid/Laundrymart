b114e07788e699bb343e83ac0b2bca1652b2185c
princid <princid85@gmail.com>
2023-06-05 18:12:08 +0530

    Implemented Date logic through moment dependency

---
+++ components/Date.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import moment from "moment";
+
+const Date = ({ date, onSelectDate, selected }) => {
+  /**
+   * use moment to compare the date to today
+   * if today, show 'Today'
+   * if not today, show day of the week e.g 'Mon', 'Tue', 'Wed'
+   */
+  const day =
+    moment(date).format("DD-MM-YYYY") === moment().format("DD-MM-YYYY")
+      ? "Today"
+      : moment(date).format("ddd");
+  // get the day number e.g 1, 2, 3, 4, 5, 6, 7
+  const dayNumber = moment(date).format("D");
+
+  // get the full date e.g 2021-01-01 - we'll use this to compare the date to the selected date
+  const fullDate = moment(date).format("DD-MM-YYYY");
+  return (
+    <TouchableOpacity
+      onPress={() => onSelectDate(fullDate)}
+      style={[
+        styles.card,
+        selected === fullDate && { backgroundColor: "#088f8f" },
+      ]}
+    >
+      <Text style={[styles.big, selected === fullDate && { color: "#fff" }]}>
+        {day}
+      </Text>
+      <View style={{ height: 10 }} />
+      <Text
+        style={[
+          styles.medium,
+          selected === fullDate && {
+            color: "#fff",
+            fontWeight: "bold",
+            fontSize: 24,
+          },
+        ]}
+      >
+        {dayNumber}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Date;
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#ffffff",
+    borderRadius: 7,
+    borderColor: "#ddd",
+    padding: 10,
+    marginVertical: 10,
+    alignItems: "center",
+    height: 90,
+    width: 80,
+    marginHorizontal: 5,
+  },
+  big: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  medium: {
+    fontSize: 16,
+  },
+});
