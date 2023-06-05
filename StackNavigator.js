0967a418f4bb304504933db380ea4d49d933c8d3
princid <princid85@gmail.com>
2023-06-05 18:07:08 +0530

    Removed some unnecessary codes and implemented navigation

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
@@ -7,46 +7,11 @@ import PickUpScreen from "./screens/PickUpScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import { User, onAuthStateChanged } from "firebase/auth";
-import { FIREBASE_AUTH } from "./firebase";
 import ProfileScreen from "./screens/ProfileScreen";
 import OrderScreen from "./screens/OrderScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
-
-  // const InsideStack = createNativeStackNavigator();
-
-  // function InsideLayout() {
-  //   return (
-  //     <InsideStack.Navigator>
-  //       <InsideStack.Screen
-  //         name="Home"
-  //         component={HomeScreen}
-  //         options={{ headerShown: false }}
-  //       />
-  //       <InsideStack.Screen
-  //         name="PickUp"
-  //         component={PickUpScreen}
-  //         options={{ headerShown: false }}
-  //       />
-  //       <InsideStack.Screen
-  //         name="Cart"
-  //         component={CartScreen}
-  //         options={{ headerShown: false }}
-  //       />
-  //     </InsideStack.Navigator>
-  //   );
-  // }
-
-  // const {user, setUser} = useState();
-
-  // useEffect(() => {
-  //   onAuthStateChanged(FIREBASE_AUTH, (user) =>{
-  //     console.log("user", user);
-  //     setUser(user);
-  //   });
-  // }, [])
 
   return (
     <NavigationContainer>
