f6136970b1c86edbd65ef10d8f746280121ce0f0
Prince Kumar Singh <90444477+princid@users.noreply.github.com>
2024-10-20 13:46:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEj4pfj8ERGbBL2KieggnxZV6Us2gcrec",
-  authDomain: "laundrymart-51e51.firebaseapp.com",
-  projectId: "laundrymart-51e51",
-  storageBucket: "laundrymart-51e51.appspot.com",
-  messagingSenderId: "667034347279",
-  appId: "1:667034347279:web:49726d1c221967c195007a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
